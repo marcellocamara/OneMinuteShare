5545c148a1996361b183781a509ab3d778163a1a
Marcello Câmara <marcellocamara@id.uff.br>
2020-01-17 20:28:59 -0300

    Feat: First commit

---
+++ app/src/main/java/dev/marcello/oneminuteshare/MainActivity.java
@@ -0,0 +1,19 @@
+package dev.marcello.oneminuteshare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by marcellocamara@id.uff.br on 17/01/2020.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
